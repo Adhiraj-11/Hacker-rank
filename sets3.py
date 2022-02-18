b0db212dae8bb3fcb31a39015a69f5ac1119042b
Adhiraj-11 <adhiraj.chauhan.2003@gmail.com>
2022-02-18 15:09:19 +0530

    ;-;

--- sets3.py
+++ sets3.py
@@ -1,17 +1,24 @@
 n = int(input())
 s = set()
 ele = input()
+
 l = ele.split(" ")
+
 for i in l:
     s.add(int(i))
+
 m = int(input())
+
 for i in range(m):
+
     inp = input()
     com = inp.split(" ")
+
     if com[0] == "pop":
         s.pop()
     elif com[0] == "remove":
         s.remove(int(com[1]))
     elif com[0] == "discard":
         s.discard(int(com[1]))
+        
 print(sum(s))
