23758e54a87986889fb6307bb20e8f092fd6a5d7
Adhiraj-11 <adhiraj.chauhan.2003@gmail.com>
2022-02-18 15:09:44 +0530

    ;-;

--- stringValidators.py
+++ stringValidators.py
@@ -1,26 +1,21 @@
 s = input()
 
-
 for i in s:
 
     if i.isalnum() is True:
         print(True)
     
         break
-
 else:
     print(False)
-
 for i in s:
 
     if i.isalpha() is True:
         print(True)
 
         break
-
 else:
     print(False)
-
 
 for i in s:
 
@@ -28,26 +23,20 @@ for i in s:
         print(True)
     
         break
-
 else:
     print(False)
-
 
 for i in s:
 
     if i.islower() is True:
         print(True)
-    
         break
-
 else:
     print(False)
 
 for i in s:
-
     if i.isupper() is True:
         print(True)
-    
         break
 
 else:
